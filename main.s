3fe17577002043409d5aaa9731c465be5844ffc3
Kevin Thomas <kevin@mytechnotalent.com>
2021-09-09 09:47:59 -0400

    Init commit

--- main.s
+++ main.s
@@ -1,42 +1,42 @@
-;//	MIT License
+;// MIT License
 ;//
-;//	Developer: Kevin Thomas
+;// Developer: Kevin Thomas
 ;//
-;//	Copyright (c) 2021 My Techno Talent, LLC
+;// Copyright (c) 2021 My Techno Talent, LLC
 ;//
-;//	Permission is hereby granted, free of charge, to any person obtaining a copy
-;//	of this software and associated documentation files (the "Software"), to deal
-;//	in the Software without restriction, including without limitation the rights
-;//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-;//	copies of the Software, and to permit persons to whom the Software is
-;//	furnished to do so, subject to the following conditions:
-;//	
-;//	The above copyright notice and this permission notice shall be included in all
-;//	copies or substantial portions of the Software.
+;// Permission is hereby granted, free of charge, to any person obtaining a copy
+;// of this software and associated documentation files (the "Software"), to deal
+;// in the Software without restriction, including without limitation the rights
+;// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;// copies of the Software, and to permit persons to whom the Software is
+;// furnished to do so, subject to the following conditions:
 ;//
-;//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-;//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-;//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-;//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-;//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-;//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-;//	SOFTWARE.
+;// The above copyright notice and this permission notice shall be included in all
+;// copies or substantial portions of the Software.
+;//
+;// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;// SOFTWARE.
 
-						AREA	PA0ButtonHandlerProg,CODE,READONLY,ALIGN=2
-						GET		STM32F401CCUx_Driver.s	
+						AREA    PA0ButtonHandlerProg,CODE,READONLY,ALIGN=2
+						GET     STM32F401CCUx_Driver.s	
 						THUMB
 						ENTRY
-						EXPORT	__main
+						EXPORT  __main
 						
 __main
-						bl		ConfigPortA
-						bl		ConfigPortC
+						bl      ConfigPortA
+						bl      ConfigPortC
 MainLoop	
-						bl		PA0ButtonHandler
-						bl		MainLoop
+						bl      PA0ButtonHandler
+						bl      MainLoop
 ConfigPortA				
-						push	{lr}
-						ldr		r0,=RCC_AHB1ENR
+						push    {lr}
+						ldr     r0,=RCC_AHB1ENR
 						ldr		r1,[r0]
 						orr		r1,#RCC_AHB1ENR_GPIOAEN
 						str		r1,[r0]
