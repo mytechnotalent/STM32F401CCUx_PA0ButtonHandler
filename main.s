135ad66f42f4dcc46f4d73abe489d913496cc92e
Kevin Thomas <kevin@mytechnotalent.com>
2021-10-29 10:00:19 -0400

    Update main.s

--- main.s
+++ main.s
@@ -2,7 +2,7 @@
 ;//
 ;// Developer: Kevin Thomas
 ;//
-;// Copyright (c) 2021 My Techno Talent, LLC
+;// Copyright (c) 2021 My Techno Talent
 ;//
 ;// Permission is hereby granted, free of charge, to any person obtaining a copy
 ;// of this software and associated documentation files (the "Software"), to deal
